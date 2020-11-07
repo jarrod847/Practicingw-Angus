d917b75e3a664c7b72bd34f80e25b34a2fbcc4b4
jarrodskahill <jarrodskahill@gmail.com>
2020-11-07 04:17:36 -0800

    setting up to calculate percentage

--- Newclass.cs
+++ Newclass.cs
@@ -4,9 +4,13 @@ namespace newC
 {
     class Newclass
     {
-        public int divide(int num1, int num2)
+        public calcPercentage(double CurrentScore, double PointsAvailable)
         {
-            return num1 / num2;
+            Program myclass = new Program();
+            double division = myclass.Divide(CurrentScore, PointsAvailable);
+            double Percent = division * 100;
+            Console.WriteLine("Your final score is " + CurrentScore + " out of " + PointsAvailable + " leaving you with " + Percent + "%");
+            return Percent;
         }
 
     }
