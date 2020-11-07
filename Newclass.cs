6f880befe65561c1c89821e5b061a0d5a25a7e3e
jarrodskahill <jarrodskahill@gmail.com>
2020-11-06 22:47:42 -0800

    practicing importing files cs

---
+++ Newclass.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace newC
+{
+    class Newclass
+    {
+        public int divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace newC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Newclass myclass = new Newclass();
+            int one = 4;
+            int two = 2;
+            string[] cars = { "Angus", "smells", "like", "shit" };
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+            Console.WriteLine(myclass.divide(one, two));
+        }
+    }
+}
